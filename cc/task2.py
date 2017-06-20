85fe26b068400ddc35b653a0af89f3ab65428554
Saburka <kasiasabura@gmail.com>
2017-06-20 18:09:10 +0200

    Update task2.py

--- cc/task2.py
+++ cc/task2.py
@@ -1,4 +1,36 @@
 # The famous polish wizard spell damage calculator
-# based on "shovel"ogy :)
 
-# under construction
+def damage(spell):
+    spell = spell.lower()  # it wasn't pointed out if upper cases are allowed or should return 0
+    beg_index = int(spell.find("fe"))  # a number of index of "fe"
+    end_index = int(spell.rfind("ai"))  # a number of index of last occurrence"ai"
+
+    subspells = {
+        "fe": 1, "je": 2, "jee": 3, "ain": 3, "dai": 5, "ne": 2, "ai": 2
+    }
+
+    if not spell.isalpha() and not spell.isspace():
+        return 0
+    elif spell.find("fe") == -1 and spell.find("ai") == -1:
+        return 0
+    elif spell.find("fe") > spell.rfind("ai") or spell.count("fe") != 1:
+        return 0
+    else:
+        spell = spell[beg_index: end_index + 2] # slice starting with "fe" and ending with last "ai"
+
+    spell = spell[2:-2]
+    part = ["jee", "je", "dai", "ne", "ain", "ai"] # the order of elements in list matters
+
+    total = subspells["fe"] + subspells["ai"]
+    for item in part:
+        if item in spell:
+            occurr = spell.count(item)
+            total = total + occurr * subspells[item]
+            spell = spell.replace(item,"")
+
+    negative_score = len(spell)
+
+    return max(total - negative_score, 0)
+
+# Let's test!
+print(damage("feaineai"))
